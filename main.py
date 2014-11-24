b8af818a46edd68d749f90e2074c35ab664311c0
yohann.roiron <yohann.roiron@mines-paristech.fr>
2014-11-24 11:21:02 +0100

    ajout des bases

---
+++ main.py
@@ -0,0 +1,7 @@
+from equipement import *
+from utilitaire import *
+import ville
+
+
+ville = new Ville()
+
---
+++ ville.py
@@ -0,0 +1,6 @@
+class ville:
+
+	def __init__(self)
+		#self.equipement=ParcEolienne()
+		#self.equipement+=Maisons()
+
