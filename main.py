c5a6cee880e23100468728b19d5bdb928d334b56
yohann.roiron <yohann.roiron@mines-paristech.fr>
2014-11-24 19:13:24 +0100

    fin debug

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from Ville import Ville
 ville = Ville()
 
 t=0 #t=0 -> Lun 00h00
-while t<1:#6*24*7:
+while t<6*24*7:
     consignesProduction=[0 for i in range(0,ville.nombreEquipementProduction-1)]
     for i in range(0,ville.nombreEquipementProduction-1):
        #actualisation des équipements de production
@@ -16,6 +16,5 @@ while t<1:#6*24*7:
     for i in range(0,ville.nombreEquipementConso-1):
         #actualisation des équipements de Consommation
         ville.equipConso.etatSuivant(i)
-    print consignesConso
     t+=1
     
