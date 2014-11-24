95ba99570e3f6740478d2e0a4c82f20817177178
14dematt <arnaud.de_mattia@mines-paristech.fr>
2014-11-24 15:31:48 +0100

    Définition de date

--- Utilitaire/heure.py
+++ Utilitaire/heure.py
@@ -1,6 +1,10 @@
 class Utilitaire:
-    def calculDate (t, mois = "Novembre", jour=1, heure=0, minute=0):
-        jour = 1 + t/60/24 
+    
+    def __init__ (self,temps):
+        date = calculDate(10*temps)
+        
+    def calculDate (t, mois = "Avril", jour=1, heure=0, minute=0):
+        jour = 1 + t/(60*24) 
         heure = (t/60) % 24
         minute = t % 60 
         date = {"Mois":mois,"Jour":jour,"Heure":heure,"Minute":minute}
