906935858ede09ca5eb82771c1aa3dfacf27ce16
yohann.roiron <yohann.roiron@mines-paristech.fr>
2014-11-24 11:21:38 +0100

    classe d'héritage

---
+++ Equipement/Equipement.py
@@ -0,0 +1,12 @@
+# -*-coding:Utf-8 -*
+class Equipement:
+	def __init__(self):
+		pass
+	def prevision(t1,etatavant,consigne):
+		""" prends en compte la période demandée (t1)
+		    l'état à t1-1: etatavant
+		    consigne pour t1"""
+	def etatSuivant(consigne):
+		"""renvoie état à partir de la consigne
+		 (t,prod,effacement,cout)"""
+		return (0,0,0,0)
