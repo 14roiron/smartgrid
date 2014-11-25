d04881ffdaab9399dba6248cfe115574a75ac474
yohann.roiron <yohann.roiron@mines-paristech.fr>
2014-11-25 08:54:06 +0100

    ajout turbine

---
+++ Equipement/TurbineAGaz.py
@@ -0,0 +1,44 @@
+class TurbineAGaz:
+    
+    def __init__(self,prod=885,effa=0,activite=0,nb=3):
+        self.PROD_MAX=prod
+        self.EFFA_MAX=effa
+        self.activite=activite
+        self.effacement=0
+        self.demarrage=[100,93.55,92.7]
+        self.arret=[100,30.8,0]
+        self.cout=self.activite/100.0*self.PROD_MAX*(0.80/1000/6)
+        
+    def prevision(self,consigne):
+        if consigne>self.activite:
+            i=0
+            while self.activite>=self.demarrage[i]:
+                i+=1
+            if consigne<self.demarrage[i+10]:
+                return (consigne,2*self.PROD_MAX*(0.80/1000/6))
+            else:
+                return (self.demarrage[i+10],2*self.PROD_MAX*(0.80/1000/6))
+        elif consigne<self.activite:
+            i=0
+            while self.activite<=self.arret[i]:
+                i+=1
+            if consigne>self.arret[i+10]:
+                return (consigne,0.5*self.cout)
+            else:
+                return (self.arret[i+10],0.5*self.cout)
+        else:
+            return(consigne,self.cout)
+        
+    def simulation(self,consigne):
+        (prodmin,coutmin)=self.prevision(0)
+        (prodmax,coutmax)=self.prevision(100)
+        return(prodmin,prodmax,coutmin,self.cout,coutmax)
+    
+    def etatSuivant(self,consigne):
+        self.activite=consigne
+    
+     
+    
+            
+
+
