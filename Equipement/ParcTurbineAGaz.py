f7b507f99279a0d1c4885af5a3fe97e07e10a50f
14dematt <arnaud.de_mattia@mines-paristech.fr>
2014-11-25 09:43:19 +0100

    Introduction d'un nombre de turbines à gaz

--- Equipement/ParcTurbineAGaz.py
+++ Equipement/ParcTurbineAGaz.py
@@ -1,13 +1,13 @@
 class TurbineAGaz:
     
-    def __init__(self,prod=885,effa=0,activite=0,nb=3):
-        self.PROD_MAX=prod
-        self.EFFA_MAX=effa
+    def __init__(self,prod=885,effa=0,activite=0,nb=4):
+        self.PROD_MAX=prod*nb
+        self.EFFA_MAX=effa*nb
         self.activite=activite
         self.effacement=0
         self.demarrage=[100,93.55,92.7]
         self.arret=[100,30.8,0]
-        self.cout=self.activite/100.0*self.PROD_MAX*(0.80/1000/6)
+        self.cout=self.activite/100.0*self.PROD_MAX*(0.80/1000/6)*nb
         
     def prevision(self,consigne):
         if consigne>self.activite:
@@ -15,9 +15,9 @@ class TurbineAGaz:
             while self.activite>=self.demarrage[i]:
                 i+=1
             if consigne<self.demarrage[i+10]:
-                return (consigne,2*self.PROD_MAX*(0.80/1000/6))
+                return (consigne,2*self.PROD_MAX*(0.80/1000/6)*nb)
             else:
-                return (self.demarrage[i+10],2*self.PROD_MAX*(0.80/1000/6))
+                return (self.demarrage[i+10],2*self.PROD_MAX*(0.80/1000/6)*nb)
         elif consigne<self.activite:
             i=0
             while self.activite<=self.arret[i]:
