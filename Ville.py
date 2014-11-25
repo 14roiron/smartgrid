ebdceee31b2dbede48eb9acc7175dce870cb99fb
14colarr <corentin.larroche@mines-paristech.fr>
2014-11-25 09:12:53 +0100

    Update Ville.py

--- Ville.py
+++ Ville.py
@@ -4,8 +4,8 @@ from Equipement.ParcMaison import ParcMaison
 class Ville:
 
 	def __init__(self):
-		self.equipProduction=ParcSolaire()
-		self.equipConso=ParcMaison()
+		self.equipProduction=[ParcSolaire()]
+		self.equipConso=[ParcMaison()]
 		self.nombreEquipementProduction=len((self.equipProduction,))
 		self.nombreEquipementConso=len((self.equipConso,))
 
