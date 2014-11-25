d95ca85d4c86b5ade91fa98ebc60df07aa700ecb
yohann.roiron <yohann.roiron@mines-paristech.fr>
2014-11-25 13:10:28 +0100

    de

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 # -*-coding:utf-8 -*
 from Equipement import *
-from Utilitaire import *
+from Utilitaire.heure import Utilitaire
 from Utilitaire.BaseDeDonnees import BaseDeDonnees
 from Ville import Ville
 
+
 """Import de la base de données"""
 
-db = BaseDeDonnee()
+db = BaseDeDonnees()
 meteo1 = db.importerTable("Semaine1")
 meteo2 = db.importerTable("Semaine2")
 meteoTest = db.importerTable("Test")
 
 """initialisation de la Ville dans l'objet Ville"""
 ville = Ville()
-
-t=0 #t=0 -> Lun 00h00
-while t<6*24*7:
+temps=0
+a=Utilitaire(temps)
+a.temps=temps #t=0 -> Lun 00h00
+while temps<6*24*7:
     # Définition des consignes de production
     conso = sum(i.PROD_MAX*(-1)*i.activite for i in ville.equipConso) # Consommation totale pour l'étape en cours
     simulations = [i.simulation() for i in ville.equipConso]
     consignesProduction=[0 for i in range(ville.nombreEquipementProduction)]
     for i in range(ville.nombreEquipementProduction):
        #actualisation des équipements de production
-       ville.equipProduction[i].etatSuivant(consignesProduction[i])
+       ville.equipProduction[i].etatSuivant(consignesProduction[i],0)
     consignesConso=[0 for i in range(ville.nombreEquipementConso)]
     for i in range(ville.nombreEquipementConso):
         #actualisation des équipements de Consommation
         ville.equipConso[i].etatSuivant()
-    t+=1
+    temps+=1
     
