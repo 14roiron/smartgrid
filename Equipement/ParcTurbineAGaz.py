dd5353814e96b0df742c8ce8befdf3b036150196
14dematt <arnaud.de_mattia@mines-paristech.fr>
2014-11-25 13:29:42 +0100

    Nombre de turbines

--- Equipement/ParcTurbineAGaz.py
+++ Equipement/ParcTurbineAGaz.py
@@ -4,9 +4,10 @@ class ParcTurbineAGaz:
         self.EFFA_MAX=effa
         self.activite=activite
         self.effacement=0
+        self.nb=nb
         self.demarrage=[0,0,0,0,22.6,56.5,67.79,74.2,81.51,96.83,100,100,100,98.46,96.04,94.43,94.43,94.43,94.43,94.43,92.33,92.33,92.33,92.33,92.33,92.33,92.33,92.33,92.33,92.33]
         self.arret=[100,100,100,100,100,100,100,100,100,30.71,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        self.cout=self.activite/100.0*self.PROD_MAX*(0.80/1000/6)
+        self.cout=self.activite/100.0*self.PROD_MAX*(0.80/1000/6)*self.nb
         
     def prevision(self,consigne):
         if consigne>self.activite:
@@ -14,9 +15,9 @@ class ParcTurbineAGaz:
             while self.activite>=self.demarrage[i]:
                 i+=1
             if consigne<self.demarrage[i+10]:
-                return (consigne,2*self.PROD_MAX*(0.80/1000/6))
+                return (consigne,2*self.PROD_MAX*(0.80/1000/6)*self.nb)
             else:
-                return (self.demarrage[i+10],2*self.PROD_MAX*(0.80/1000/6))
+                return (self.demarrage[i+10],2*self.PROD_MAX*(0.80/1000/6)*self.nb)
         elif consigne<self.activite:
             i=0
             while self.activite<=self.arret[i]:
