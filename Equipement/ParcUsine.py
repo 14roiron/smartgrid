67e5f16f8f3abd14a82276d64ef36bf46ba192af
14dematt <arnaud.de_mattia@mines-paristech.fr>
2014-11-25 13:31:53 +0100

    changement nom attribut

--- Equipement/ParcUsine.py
+++ Equipement/ParcUsine.py
@@ -1,5 +1,3 @@
-from Utilitaire import Utilitaire
-
 class ParcUsine(Equipements):
     
     global temps
@@ -7,10 +5,10 @@ class ParcUsine(Equipements):
     def __init__(self,prod=3000.0,effa=1000.0,activite=0,nb=2) :
         self.PROD_MAX=prod*nb #en kW, c'est une puissance instantanée
         self.EFFA_MAX=effa*nb
-        self.nombreUsines=nb
+        self.nb=nb
         self.activite=activite
         self.effacement=0.0
-        self.cout=self.effacement/100.0*self.EFFA_MAX*(0.80/1000/6)*self.nombreUsines
+        self.cout=self.effacement/100.0*self.EFFA_MAX*(0.80/1000/6)*self.nb
         self.prod=[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 25.0, 50.0, 75.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 75.0, 50.0, 25.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
         
     
@@ -24,9 +22,9 @@ class ParcUsine(Equipements):
     def prevision(self,consigne=0,effacement=0):
         p=prod[(temps+1)%7]
         if p>=effacement*EFFA_MAX/PROD_MAX:
-            return (p-effacement*EFFA_MAX/PROD_MAX,effacement/100.0*self.EFFA_MAX*(0.80/1000/6)*self.nombreUsines)
+            return (p-effacement*EFFA_MAX/PROD_MAX,effacement/100.0*self.EFFA_MAX*(0.80/1000/6)*self.nb)
         else :
-            return (0,p/100.0*self.PROD_MAX*(0.80/1000/6)*self.nombreUsines)
+            return (0,p/100.0*self.PROD_MAX*(0.80/1000/6)*self.nb)
     
     def simulation(self):
         (prod_min,cout_min)=self.prevision(0,0)  
