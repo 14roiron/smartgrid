8141aabe147de1218f13a779c4abad9d31ae709c
14alarro <14alarro@users.noreply.github.com>
2014-11-26 09:39:05 +0100

    Update Global.py

--- Utilitaire/Global.py
+++ Utilitaire/Global.py
@@ -1,9 +1,9 @@
 from Utilitaire.BaseDeDonnees import BaseDeDonnees
 temps=0
 db = BaseDeDonnees()
-meteo1 = db.importerTable("Semaine1")
-meteo2 = db.importerTable("Semaine2")
-meteoTest = db.importerTable("Test")
+meteo1 = db.importerMeteo("Semaine1")
+meteo2 = db.importerMeteo("Semaine2")
+meteoTest = db.importerMeteo("Test")
 def tempsinc():
     global temps
     temps+=1
