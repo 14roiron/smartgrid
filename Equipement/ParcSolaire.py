95eb0c2e3efff1668a83be8ddab393fdba9f6107
14thibea <elina.thibeau-sutre@mines-paristech.fr>
2014-11-26 09:32:42 +0100

    changement dans test

--- Equipement/ParcSolaire.py
+++ Equipement/ParcSolaire.py
@@ -6,20 +6,24 @@ from Utilitaire.Global import meteoTest
 
 class ParcSolaire(Equipement):
     
-    def __init__(self,nom="ParcSolaire", prod=150, effa=0, activite=10, nb=50, meteo=meteoTest):
-        Equipement.__init__(self,nom,prod,effa,activite)
+    def __init__(self, prod=150, activite=10, nb=50, meteo=meteoTest, nom="ParcSolaire"):
         '''nombre de panneaux solaires dans la ferme'''
         self.nb = nb
         '''Trois possibilités : meteo1, meteo2 ou meteoTest'''
         self.meteo = meteo
+        '''nom du Parc'''
+        self.nom = nom
         
         self.PROD_MAX = self.nb*prod
+        self.activite = activite
+        self.EFFA_MAX = 0
+        self.effacement = 0
         
     def prevision(self, consigne, effacement):
         """retourne l'activité à l'état suivant en pourcentage par rapport à PROD_MAX"""
         return (self.calculActivite(Global.temps+1), 0)
     
-    def simulation(self,activite,effacement):
+    def simulation(self):
         """pas de consigne ou d'effacement possible pour un panneau solaire :
         puissance min = puissance max et le coût est toujours le même (que le panneau produise ou pas)"""
         temps= Global.temps
@@ -39,14 +43,15 @@ class ParcSolaire(Equipement):
         else:
             return False
     
-    def calculActivite(self,temps):
+    def calculActivite(self):
         """formule de test, lien avec les données météo à faire"""
+        temp=Global.temps
         return self.meteo[temps]["GHI"] / 1000 * 100
         
 #pour les tests
 if __name__=='__main__':
     a=ParcSolaire()
     a.simulation()
-    a.calculActivite()
+    a.contraintes(60,0)
     
 #des tests sont à effectuer pour vérifier le comportement en profondeur mais ça semble ok!
