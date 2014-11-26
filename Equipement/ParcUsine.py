9703c2c6389e676ae316038b91194a900c49cfa3
14dematt <arnaud.de_mattia@mines-paristech.fr>
2014-11-26 08:42:07 +0100

    changement cout

--- Equipement/ParcUsine.py
+++ Equipement/ParcUsine.py
@@ -6,29 +6,29 @@ class ParcUsine(Equipement):
     
     global temps
     
-    def __init__(self,prod=3000.0,effa=1000.0,activite=0,nb=2) :
+    def __init__(self,nom,prod=3000.0,effa=1000.0,activite=0,nb=2) :
         self.PROD_MAX=prod*nb #en kW, c'est une puissance instantanée
         self.EFFA_MAX=effa*nb
-        self.nb=nb
+        self.nombre=nb
         self.activite=activite
         self.effacement=effa
-        self.cout=self.effacement/100.0*self.EFFA_MAX*(0.80/1000/6)*self.nb
-        self.prod=[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 25.0, 50.0, 75.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 75.0, 50.0, 25.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-        
+        self.cout=self.effacement/100.0*self.EFFA_MAX*(80/1000/6)*self.nombre
+        self.nom=nom
+        self.production=[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 25.0, 50.0, 75.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 75.0, 50.0, 25.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
     
     def etatSuivant(self,consigne=0,effacement=0):
-        p=self.prod[temps%144]
+        p=self.production[temps%144]
         if p>=effacement*self.EFFA_MAX/self.PROD_MAX:
             activite=p-effacement*self.EFFA_MAX/self.PROD_MAX
         else:
             activite=0.0
         
     def prevision(self,consigne=0,effacement=0):
-        p=self.prod[(temps+1)%144]
+        p=self.production[(temps+1)%144]
         if p>=effacement*self.EFFA_MAX/self.PROD_MAX:
-            return (p-effacement*self.EFFA_MAX/self.PROD_MAX,effacement/100.0*self.EFFA_MAX*(0.80/1000/6)*self.nb)
+            return (p-effacement*self.EFFA_MAX/self.PROD_MAX,effacement/100.0*self.EFFA_MAX*(80/1000/6)*self.nombre)
         else :
-            return (0,p/100.0*self.PROD_MAX*(0.80/1000/6)*self.nb)
+            return (0,p/100.0*self.PROD_MAX*(80/1000/6)*self.nombre)
     
     def simulation(self):
         (prod_min,cout_min)=self.prevision(0,0)  
