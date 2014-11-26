b345436e264e5a3def0b8398480807c9995a8841
adrientardieu <tardieu.adrien@gmail.com>
2014-11-26 22:39:16 +0100

    lolilol

--- Equipement/ParcMaison.py
+++ Equipement/ParcMaison.py
@@ -4,34 +4,42 @@ from Utilitaire.heure import Utilitaire
 from Utilitaire import Global
 from math import *
 
+'''INFOS
+    production = - consommation ; 
+    prod_min = - conso_max = conso sans effacement ;
+    prod_max = - conso_min = conso avec effacement ; => cout_max = cout pour effacer
+    effacement = prod_max - prod_min 
+    attention prod_min = equipement.activite = -conso
+'''
+
 class ParcMaison (Utilitaire) : 
-    def __init__(self,nom,prod=-2.0,effa=0.1,activite=0,nb=300): #consommation moyenne de environ 1kW/maison -->heure basse 0,7kW/maison
+    def __init__(self, nom, prod=-2.0, effa=0.1, activite=0, nb=300): #consommation moyenne de environ 1kW/maison -->heure basse 0,7kW/maison
         self.nombre=nb
-        self.PROD_MAX=prod*self.nombre  # consommation de 2kW par maison (pic)
+        self.PROD_MAX=prod*self.nombre  # consommation de 2kW par maison (pic) Attention production toujours négative
         self.EFFA_MAX=effa*self.nombre # en kWglobal
         self.activite=activite
         self.effacement=0.0 # en %
         self.cout=self.effacement/100.0*self.EFFA_MAX*(80/1000/6)*self.nombre
         self.nom=nom
-        prod=[]
+        
+        self.production=[]
         for i in range(0,721):
-            prod.append(50*(1+cos(pi/144.0*(i+30.0))*cos(3.0*(pi/144*(i+30)))))
+            self.production.append(50*(1+cos(pi/144.0*(i+30.0))*cos(3.0*(pi/144*(i+30))))) #pourcentage qui multiplié par self.PROD_MAX (<0) donne la production (<0)
         for i in range(721,1008):
-            prod.append(50*(1+cos(pi/72*(i-792))))
-        self.production=prod
+            self.production.append(50*(1+cos(pi/72*(i-792))))
     
-    def etatSuivant(self,consigne=0,effacement=0):
-        p=self.production[Global.temps]
-        if p>=-effacement*self.EFFA_MAX/self.PROD_MAX:
+    def etatSuivant(self, consigne=0, effacement=0):
+        p = self.consommation[Global.temps] #% de la production à l'étape actuelle, >0
+        if p >= -effacement*self.EFFA_MAX/self.PROD_MAX: #ie p * PROD_MAX <= -eff * EFFA_MAX ie consommation plus grande l'effacement demandé
             self.effacement=effacement
-            self.activite=p+effacement*self.EFFA_MAX/self.PROD_MAX
+            self.activite=p+effacement*self.EFFA_MAX/self.PROD_MAX #maj de l'activité
         else:
-            self.effacement=self.activite
+            self.effacement=self.activite #sinon on coupe totalement la consommation en faisant l'effacement maximal possible
             self.activite=0.0
         self.cout=self.effacement/100.0*self.EFFA_MAX*(80/1000/6)*self.nombre
         
-    def prevision(self,consigne=0,effacement=0):
-        p=self.production[(Global.temps+1)%1008]
+    def prevision(self, consigne=0, effacement=0):
+        p=self.consommation[(Global.temps+1)%1008] #si l'effacement demandé est inférieur à la consommation...
         if p>=-effacement*self.EFFA_MAX/self.PROD_MAX:
             return (p+effacement*self.EFFA_MAX/self.PROD_MAX,effacement/100.0*self.EFFA_MAX*(80/1000/6)*self.nombre)
         else :
