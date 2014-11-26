1208df5b81f1fe9778cfd28317fec8b91a54fae1
adrientardieu <adrien.tardieu@mines-paristech.fr>
2014-11-26 20:18:37 +0100

    update2

--- main.py
+++ main.py
@@ -20,12 +20,6 @@ ville = Ville()
 
 temps=0 #t=0 -> Lun 00h00
 
-exemple_conso_j = [21,22,23,23,24,24,25,25,25,24,25,26,27,28,28,29,30,30,30,29,28,27,27,28,28,29,30,30,30,32,34,36,38,40,\
-42,44,46,47,48,48,48,49,50,51,52,53,54,55,55,56,56,57,56,55,54,56,57,57,56,55,54,53,50,48,47,46,45,45,45,\
-45,45,45,46,47,46,45,45,47,48,49,50,50,50,51,52,51,50,51,51,50,51,53,54,57,59,60,62,62,68,69,71,73,75,77,78,\
-78,77,78,79,80,82,84,82,80,79,79,80,78,75,73,70,68,65,63,60,58,57,55,55,56,54,50,48,45,43,40,38,35,33,50,28,\
-26,23,20]
-
 def ind_eqpascher(liste,consigne): #indice de l'equipement le moins cher, liste comme simulations
     i=0
     cout_min = liste[0][4]
@@ -42,8 +36,8 @@ while Global.temps<6*24*7: #boucle principale
     """conso = sum(i.PROD_MAX*(-1)*i.activite for i in ville.equipConso) # Consommation totale pour l'étape en cours"""
     simulations = [i.simulation() for i in ville.equipProduction] #liste représentant les equipements de production pour l'etape suivante
 
-    prod_actuelle = sum((i.PROD_MAX)*i.activite for i in ville.equipProduction)
-    conso_future = 1000. #a modifier
+    prod_actuelle = sum(i.activite*i.PROD_MAX for i in ville.equipProduction)
+    conso_future = sum(i.activite*i.PROD_MAX for i in ville.equipConso)
 
     diff=conso_future-prod_actuelle # différence conso-production actuelle
     consigne = [i.activite for i in ville.equipProduction] # liste des consignes equipements de production
