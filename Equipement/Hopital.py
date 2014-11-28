faffe38994183b13ec33d7c7ce2e3dcdd25c6b02
14roiron <yohann.roiron@mines-paristech.fr>
2014-11-28 22:03:02 +0100

    correction encodage

--- Equipement/Hopital.py
+++ Equipement/Hopital.py
@@ -1,4 +1,4 @@
-# -*- coding : UTF-8 -*-
+## -*-coding:utf-8 -
 
 from Equipement import Equipement
 from Utilitaire.heure import Utilitaire
@@ -12,15 +12,15 @@ class Hopital(Equipement):
         for i in range(7):
             self.production = self.production + [50. for i in range(43)] + [60.] + [70. for i in range(69)] + [60.] + [50. for i in range(30)]
         self.effacement = 0.
-        self.temps_effa = 0 # temps pendant lequel l'hôpital s'est effacé
-        self.temps_dernier_effa = 0 # temps écoulé depuis le dernier effacement
-        self.cout = 10. # coût arbitraire, de l'ordre de celui de 100 maisons : on efface l'hôpital le moins souvent possible
+        self.temps_effa = 0 # temps pendant lequel l'hÃ´pital s'est effacÃ©
+        self.temps_dernier_effa = 0 # temps Ã©coulÃ© depuis le dernier effacement
+        self.cout = 10. # coÃ»t arbitraire, de l'ordre de celui de 100 maisons : on efface l'hÃ´pital le moins souvent possible
         
 
     def simulation(self):
         prod_min = self.production[Global.temps + 1] # pas d'effacement 
-        if (self.temps_effa >= 3 or self.temps_dernier_effa <18):  # on ne peut pas effacer l'hôpital pendant plus de 30 minutes et il faut attendre 3h avant de l'effacer de nouveau
-            prod_max = prod_min                                    #j'aime l'idée
+        if (self.temps_effa >= 3 or self.temps_dernier_effa <18):  # on ne peut pas effacer l'hÃ´pital pendant plus de 30 minutes et il faut attendre 3h avant de l'effacer de nouveau
+            prod_max = prod_min                                    #j'aime l'idÃ©e
             cout_max = 0
         else:
             prod_max = self.production[Global.temps + 1] - self.EFFA_MAX/self.PROD_MAX*100
