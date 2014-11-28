12d482caf47e21ec3f185d158bb0ccfbcc233873
14dematt <arnaud.de_mattia@mines-paristech.fr>
2014-11-28 16:28:50 +0100

    Dont touch !

--- Equipement/ParcUsine.py
+++ Equipement/ParcUsine.py
@@ -5,45 +5,40 @@ from Utilitaire import Global
 
 class ParcUsine(Equipement):
     
-    def __init__(self,nom="usine",prod=-3000.,effa=1000.,activite=0.,nb=2.) :
-        self.nombre=nb
+    def __init__(self,nom="usine",prod=-3000.,effa=1000.,activite=0.,nombre=2) :
+        self.nombre=nombre
         self.PROD_MAX=prod*self.nombre #en kW, c'est une puissance instantanée
         self.EFFA_MAX=effa*self.nombre
         self.activite=activite
         self.effacement=0.
         self.cout=self.effacement/100.*self.EFFA_MAX*(80./1000./6.)*self.nombre
         self.nom=nom
-        
-        courbe = [0. for i in range(144)]
-        courbe[43] = 25.
-        courbe[44] = 50. 
-        courbe[45] = 75. 
+        self.production = [0. for i in range(144)]
+        self.production[43] = 25.
+        self.production[44] = 50. 
+        self.production[45] = 75. 
         for i in range(46,117):
-            courbe[i] = 100.
-        courbe[117] = 75. 
-        courbe[118] = 50.
-        courbe[119] = 25. 
-        self.production = []
-        for i in range(7):
-            self.production += courbe
-        
+            self.production[i] = 100.
+        self.production[117] = 75. 
+        self.production[118] = 50.
+        self.production[119] = 25. 
     
-    def etatSuivant(self,consigne=0.,effacement=0.):#WTF ? act=0
-        p=self.production[Global.temps%144]
-        if p>=-effacement*self.EFFA_MAX/self.PROD_MAX:
+    def etatSuivant(self,consigne=0.,effacement=0.):
+        pourcentage=self.production[Global.temps%144]
+        if pourcentage>=-effacement*self.EFFA_MAX/self.PROD_MAX:
             self.effacement=effacement
-            self.activite=p+effacement*self.EFFA_MAX/self.PROD_MAX
+            self.activite=pourcentage+effacement*self.EFFA_MAX/self.PROD_MAX
         else:
             self.effacement=self.activite
             self.activite=0.
         self.cout=self.effacement/100.*self.EFFA_MAX*(80./1000./6.)*self.nombre
         
     def prevision(self,consigne=0.,effacement=0.):
-        p=self.production[(Global.temps+1)%144]
-        if p>=-effacement*self.EFFA_MAX/self.PROD_MAX:
-            return (p+effacement*self.EFFA_MAX/self.PROD_MAX,effacement/100.*self.EFFA_MAX*(80./1000./6.)*self.nombre)
+        pourcentage=self.production[(Global.temps+1)%144]
+        if pourcentage>=-effacement*self.EFFA_MAX/self.PROD_MAX:
+            return (pourcentage+effacement*self.EFFA_MAX/self.PROD_MAX,effacement/100.*self.EFFA_MAX*(80./1000./6.)*self.nombre)
         else :
-            return (0.,-p/100.*self.PROD_MAX*(80./1000./6.)*self.nombre)
+            return (0.,-pourcentage/100.*self.PROD_MAX*(80./1000./6.)*self.nombre)
     
     def simulation(self):
         (prod_min,cout_min)=self.prevision(0.,0.)  
