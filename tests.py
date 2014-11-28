1167e47a6d4cfb133d66584390f006dcdb24b5c5
adrientardieu <tardieu.adrien@gmail.com>
2014-11-28 15:29:37 +0100

    petites modifs

--- tests.py
+++ tests.py
@@ -19,29 +19,29 @@ ville = Ville()
 
 temps=0 #t=0 -> Lun 00h00
 Global.db.vide_table()
-Global.db.enregistrerID(ville.equipProduction, ville.equipConso, 0)
+Global.db.enregistrerID(ville.equipProduction, ville.equipConso, ville.equipStockage, 0)
 #print len(Global.meteo1)
 print len(Global.meteo2)
 while Global.temps < 6*24*7: #boucle principale
 
-    consigne=[100 for i in range(len(ville.equipProduction))]
-    consigne_stock=[100 for i in (range(len(ville.equipStockage)))]
-    consigne_conso=[100 for i in (range(len(ville.equipConso)))]
+    consigne=[100. for i in range(len(ville.equipProduction))]
+    consigne_stock=[100. for i in (range(len(ville.equipStockage)))]
+    consigne_conso=[100. for i in (range(len(ville.equipConso)))]
     
     for i in range(len(consigne)):
-        #print i.nom
-        ville.equipProduction[i].etatSuivant(consigne[i],0)
+        
+        ville.equipProduction[i].etatSuivant(consigne[i],0.)
     
     for i in range(len(consigne_stock)):
-        #print i.nom
-        ville.equipStockage[i].etatSuivant(consigne_stock[i],0)
+        
+        ville.equipStockage[i].etatSuivant(consigne_stock[i],0.)
     
     for i in range(len(consigne_conso)):
-        #print i.nom
-        ville.equipConso[i].etatSuivant(0,consigne_conso[i])
+        
+        ville.equipConso[i].etatSuivant(0.,consigne_conso[i])
     
 
-    Global.db.enregistrerEtape(ville.equipProduction, ville.equipConso, 0)        
+    Global.db.enregistrerEtape(ville.equipProduction, ville.equipConso, ville.equipStockage, 0)        
     Global.tempsinc()#temps+=1
     print Global.temps
     
