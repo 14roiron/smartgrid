1e98e2149a69f6d5fd7eba7b0c51009a9137d847
adrientardieu <tardieu.adrien@gmail.com>
2014-11-29 01:06:15 +0100

    mouais

--- Equipement/ParcTurbineAGaz.py
+++ Equipement/ParcTurbineAGaz.py
@@ -1,33 +1,33 @@
 # -*-coding:utf-8 -
 class ParcTurbineAGaz:
-    def __init__(self,nom="turbine_a_gaz",prod=1000.,effa=0.,activite=0.,nombre=12.):
+    def __init__(self,nom="turbine_a_gaz",prod=10000.,effa=0.,activite=0.,nombre=2.):
         self.nom=nom
         self.nombre=nombre
         self.PROD_MAX=prod*self.nombre
         self.EFFA_MAX=effa
         self.activite=activite
         self.effacement=0.
-        self.demarrage=[0.,0.,0.,0.,22.6,56.5,67.79,74.2,81.51,96.83,100.,100.,100.,98.46,96.04,94.43,94.43,94.43,94.43,94.43,92.33,92.33,92.33,92.33,92.33,92.33,92.33,92.33,92.33,92.33] #courbe de montée en puissance, pas de 1 min
-        self.arret=[100.,100.,100,100.,100.,100.,100.,100.,100.,30.71,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.] #courbe d'arrêt, pas de 1 min
+        self.demarrage=[22.6,32.,56.5,67.79,74.2,81.51,96.83,100.,100.,100.,98.46,96.04,94.43,94.43,94.43,94.43,94.43,92.33,92.33,92.33,92.33,92.33,92.33,92.33,92.33,92.33,92.33] #courbe de montée en puissance, pas de 1 min
+        self.arret=[50.,30.71,0.,0.] #courbe d'arrêt
         self.cout=self.activite/100.*self.PROD_MAX*(80./1000./6.)*self.nombre
         
     def prevision(self,consigne=0.,effacement=0.):
-        if consigne>self.activite: #si on veut augmenter la puissance, on place l'activité actuelle sur la courbe de montée en puissance et on en déduit l'état à t+10min
+        if consigne > self.activite: #si on veut augmenter la puissance, on place l'activité actuelle sur la courbe de montée en puissance et on en déduit l'état à t+10min
             i=0
-            while self.activite>self.demarrage[i]:
+            while self.activite > self.demarrage[i]:
                 i+=1
-            if consigne<self.demarrage[i+10]: #si l'activité demandée est atteinte avant t+10min
+            if consigne < self.demarrage[i+1]: #si l'activité demandée est atteinte avant t+10min
                 return (consigne,2*self.PROD_MAX*(80./1000./6.)*self.nombre) #nous avons supposé pour l'instant qu'il est deux fois plus cher de mettre la turbine en marche que de l'arrêter, ce qui explique le facteur "2". 
             else:
-                return (self.demarrage[i+10],2*self.PROD_MAX*(80./1000./6.)*self.nombre) #même chose avec l'arrêt
-        elif consigne<self.activite:
+                return (self.demarrage[i+1],2*self.PROD_MAX*(80./1000./6.)*self.nombre) #même chose avec l'arrêt
+        elif consigne < self.activite:
             i=0
-            while self.activite<self.arret[i]:
+            while self.activite < self.arret[i]:
                 i+=1
-            if consigne>self.arret[i+10]:
+            if consigne > self.arret[i+1]:
                 return (consigne,0.5*self.cout) #voir 8 lignes plus haut pour avoir l'explication du facteur "0.5". c'est assez imprécis comme raisonnement mais pour le moment, il nous manque les données relatives au calcul de ces coûts
             else:
-                return (self.arret[i+10],0.5*self.cout)
+                return (self.arret[i+1],0.5*self.cout)
         else: #si on garde une activité constante
             return(consigne,self.cout)
             
@@ -41,8 +41,10 @@ class ParcTurbineAGaz:
 
 if __name__=="__main__":
     a=ParcTurbineAGaz()
-    a.simulation()
+    print a.simulation()
+    a.etatSuivant(100.,0.)
     print a.activite
-    a.etatSuivant(100,100)
+    print a.simulation()
+    a.etatSuivant(100.,0.)
     print a.activite
-    print a.prevision(100,100)
+    print a.simulation()
