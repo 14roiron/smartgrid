a924c49dd3663eab5afdc3d7198c9fec34a1907a
yohann.roiron <yohann.roiron@mines-paristech.fr>
2014-12-03 08:37:05 +0100

    ajout foisonnement

--- Utilitaire/Global.py
+++ Utilitaire/Global.py
@@ -6,6 +6,9 @@ duree=6*24
 db = BaseDeDonnees()
 meteo1 = db.importerMeteo("Semaine1")
 meteo2 = db.importerMeteo("Semaine2")
+meteo3 = db.importerMeteo("Semaine3")
+meteo4 = db.importerMeteo("Semaine4")
+meteo5 = db.importerMeteo("Semaine5")
 meteoTest = db.importerMeteo("Test")
 def tempsinc():
     global temps
--- Ville.py
+++ Ville.py
@@ -10,16 +10,26 @@ from Equipement.ParcBatterieLithiumIon import ParcBatterieLithiumIon
 from Equipement.ParcMagasins import ParcMagasins
 from Utilitaire.Global import meteo1
 from Utilitaire.Global import meteo2
+from Utilitaire.Global import meteo3
+from Utilitaire.Global import meteo4
+from Utilitaire.Global import meteo5
 from Utilitaire.Global import meteoTest
 
 class Ville:
 
 	def __init__(self):
 		self.equipProduction = [ParcSolaire(nom="PVmeteo1",prod=250.,activite=50.,nb=10.,meteo=meteo1),\
-		                        ParcSolaire("PVmeteo2",prod=250.,activite=50.,nb=10.,meteo=meteo2),\
-		                        ParcTurbineAGaz("turbine"),\
+		                        ParcSolaire(nom="PVmeteo2",prod=250.,activite=50.,nb=10.,meteo=meteo2),\
+		                        ParcSolaire(nom="PVmeteo3",prod=250.,activite=50.,nb=10.,meteo=meteo3),\
+		                        ParcSolaire(nom="PVmeteo4",prod=250.,activite=50.,nb=10.,meteo=meteo4),\
+		                        ParcSolaire(nom="PVmeteo5",prod=250.,activite=50.,nb=10.,meteo=meteo5),\
+		                        ParcTurbineAGaz("turbine1"),\
+		                        ParcTurbineAGaz("turbine2"),\
 		                        ParcEolien(nom="eolienne,meteo1",n=500., eolienne="eolienne5", meteoVent=meteo1),\
-		                        ParcEolien(nom="eolienne,meteo2",n=5., eolienne="eolienne1500", meteoVent=meteo2)]
+		                        ParcEolien(nom="eolienne,meteo2",n=5., eolienne="eolienne1500", meteoVent=meteo2),\
+		                        ParcEolien(nom="eolienne,meteo3",n=500., eolienne="eolienne5", meteoVent=meteo3),\
+		                        ParcEolien(nom="eolienne,meteo4",n=5., eolienne="eolienne1500", meteoVent=meteo4),\
+		                        ParcEolien(nom="eolienne,meteo5",n=500., eolienne="eolienne5", meteoVent=meteo5)]
 		self.equipConso = [ParcUsine38("Usine2-38"),\
 						   ParcMaison("parcmaison1",nombre=1200),\
 		                   ParcUsine("Usine1"),\
