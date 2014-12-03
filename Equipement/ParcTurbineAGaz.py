0e37bc697279cb8863328e0d5a5e60c16336b034
14dematt <arnaud.de_mattia@mines-paristech.fr>
2014-12-03 11:37:55 +0100

    modif cout

--- Equipement/ParcTurbineAGaz.py
+++ Equipement/ParcTurbineAGaz.py
@@ -10,29 +10,29 @@ class ParcTurbineAGaz:
         self.effacement=0.
         self.demarrage=[0.,0.,0.,0.,22.6,32.,56.5,67.79,74.2,81.51,96.83,100.,100.,100.,98.46,96.04,94.43,94.43,94.43,94.43,94.43,92.33,92.33,92.33,92.33,92.33,92.33,92.33,92.33,92.33,92.33] #courbe de montée en puissance, pas de 1 min
         self.arret=[100.,100.,100.,100.,100.,100.,100.,100.,100.,30.71,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.] #courbe d'arrêt
-        self.cout=(self.varcout*self.activite/100.*self.PROD_MAX*(19.30/1000./6.)+19./6.+53.65/6.)*self.nombre
+        self.cout=self.varcout*self.activite/100.*self.PROD_MAX*(80./1000./6.)*self.nombre
         
     def prevision(self,consigne=0.,effacement=-1.):
         if consigne==0 and effacement==-1:
-            return (self.activite,(self.varcout*self.activite/100.*self.PROD_MAX*(80./1000./6.)+19./6.+53.65/6.)*self.nombre)
+            return (self.activite,(self.varcout*self.activite/100.*self.PROD_MAX*(80./1000./6.)*self.nombre)
         elif consigne > self.activite: #si on veut augmenter la puissance, on place l'activité actuelle sur la courbe de montée en puissance et on en déduit l'état à t+10min
             i=0
             while self.activite > self.demarrage[i]:
                 i+=1
             if consigne < self.demarrage[i+10]: #si l'activité demandée est atteinte avant t+10min
-                return (consigne,(2.*self.varcout*consigne/100.*self.PROD_MAX*(80./1000./6.)+19./6.+53.65/6.)*self.nombre) #nous avons supposé pour l'instant qu'il est deux fois plus cher de mettre la turbine en marche que de garder un fonctionnement constant, ce qui explique le facteur "2". 
+                return (consigne,2.*self.varcout*consigne/100.*self.PROD_MAX*(80./1000./6.)*self.nombre) #nous avons supposé pour l'instant qu'il est deux fois plus cher de mettre la turbine en marche que de garder un fonctionnement constant, ce qui explique le facteur "2". 
             else:
-                return (self.demarrage[i+10],(2.*self.varcout*self.demarrage[i+10]/100.*self.PROD_MAX*(80./1000./6.)+19./6.+53.65/6.)*self.nombre) 
+                return (self.demarrage[i+10],2.*self.varcout*self.demarrage[i+10]/100.*self.PROD_MAX*(80./1000./6.)*self.nombre) 
         elif consigne < self.activite: #même chose avec l'arrêt
             i=0
             while self.activite < self.arret[i]:
                 i+=1
             if consigne > self.arret[i+10]:
-                return (consigne,(0.5*self.varcout*self.activite/100.*self.PROD_MAX*(80./1000./6.)+19./6.+53.65/6.)*self.nombre) #voir 8 lignes plus haut pour avoir l'explication du facteur "0.5". c'est assez imprécis comme raisonnement mais pour le moment, il nous manque les données relatives au calcul de ces coûts
+                return (consigne,0.5*self.varcout*self.activite/100.*self.PROD_MAX*(80./1000./6.)*self.nombre) #voir 8 lignes plus haut pour avoir l'explication du facteur "0.5". c'est assez imprécis comme raisonnement mais pour le moment, il nous manque les données relatives au calcul de ces coûts
             else:
-                return (self.arret[i+10],(0.5*self.varcout*self.activite/100.*self.PROD_MAX*(80./1000./6.)+19./6.+53.65/6.)*self.nombre)
+                return (self.arret[i+10],0.5*self.varcout*self.activite/100.*self.PROD_MAX*(80./1000./6.)*self.nombre)
         else: #si on garde une activité constante
-            return (consigne,(self.varcout*self.activite/100.*self.PROD_MAX*(80./1000./6.)+19./6.+53.65/6.)*self.nombre)
+            return (consigne,self.varcout*self.activite/100.*self.PROD_MAX*(80./1000./6.)*self.nombre)
             
     def simulation(self):
         (prodmin,coutmin)=self.prevision(0.,0.)
