d5d88831f0cdbad9082339ecd7bbb9fbf44e93d0
14thibea <elina.thibeau-sutre@mines-paristech.fr>
2014-12-03 12:07:11 +0100

    this is the eeeeeend

--- Equipement/ParcBatterieLithiumIon.py
+++ Equipement/ParcBatterieLithiumIon.py
@@ -11,11 +11,10 @@ si c'est possible elle stockera gentiment une puissance de abs(consigne)/100 * P
 
         Les impossibilités rencontrées par les batteries sont :
 
+- la batterie a plusieurs modes de chargement : rapide, semi-rapide ou lent.
 - la surtension (dégradation de la batterie = très cher)
-- la sous tension extensive (destruction de la batterie, rendu impossible par le modèle)
+- la sous tension (destruction de la batterie, rendu impossible par le modèle)
 - le manque de capacité pour stocker/le manque de stock à déstocker'''
-
-#chocolatine
 
 class ParcBatterieLithiumIon:
     
@@ -27,16 +26,18 @@ class ParcBatterieLithiumIon:
         self.capacite = 6.5*self.nombre
         '''énergie stockée dans le parc en kWh'''
         self.reste = self.capacite*prop #pas un pourcentage
-        '''production maximale en kW'''
-        self.PROD_MAX = 24.*self.nombre
-        self.COUT_MAX = 0.7
+        '''production maximale en kW''' #pour la charge rapide
+        self.PROD_MAX = 23.*self.nombre
         '''production normale en kW'''
         self.PROD_NOR = 20.*self.nombre
-        self.COUT_NOR = 0.1
         '''production minimale en kW'''
         self.PROD_MIN = 16.*self.nombre
-        self.COUT_MIN = 0.1
+        self.coefficient_de_charge = 0
+        self.choix_possibles_de_charge = [1,1/2,1/4,0]
         '''Les prix sont en €/kWh'''
+        self.cout_charge_rapide = 0.9
+        self.cout_charge_semi_rapide = 0.5
+        self.cout_charge_lente = 0.1
         
         '''EFFACEMENT minimale en kW'''
         self.EFFA_MAX = self.PROD_MAX
@@ -46,34 +47,40 @@ class ParcBatterieLithiumIon:
         
         '''l'activité est en %'''
         self.activite = activite
+        
         '''les compteurs permet d'éviter une trop longue mise en surtension. Au bout
         de 2 pas la batterie se voit obliger d'arrêter le plein régime pour 8 pas de repos'''
-        self.compteur_surtension = 0
-        self.compteur_pause = 4
+        self.compteur_fin = 0
+        self.compteur_pause = 3
         
     def etat_suivant(self, consigne=0.): #consigne en pourcentage de PROD_MAX
-        '''si la pile est depuis trop longtemps en surtension elle s'arrête'''
-        if self.compteur_surtension == 1:
+        '''si la pile a fini une dé/charge elle s'arrête'''
+        if self.compteur_fin == 1:
             self.compteur_pause = 0
-            self.compteur_surtension = 0
+            self.compteur_fin = 0
             self.activite = 0
+            self.coefficient_de_charge = 0
             
             '''si la pile est en mode pause et qu'elle doit le prolonger'''
-        elif self.compteur_pause < 4:
+        elif self.compteur_pause < 2:
             self.compteur_pause += 1
             self.activite = 0
+            self.coefficient_de_charge = 0
         
             '''sinon si elle est en état de fonctionnement on éxécute la prévision'''
         else:
             self.activite = self.prevision(consigne)[0]
-            if abs(self.activite) >= 95:
-                self.compteur_surtension += 1
+            if self.activite == 0:
+                self.compteur_fin += 1
+                self.type_de_charge = 0
+            else:
+                self.type_de_charge = self.calculType(self.activite)
             self.reste = self.reste + self.activite*self.PROD_MAX/6/100
         
     def prevision(self, consigne=0.): #consigne en pourcentage
         
         '''si la pile entre ou continue son mode de pause'''
-        if self.compteur_surtension == 1 or self.compteur_pause < 4:
+        if self.compteur_fin == 1 or self.compteur_pause < 2:
             return (0.,0.)
         
         elif self.contraintes(consigne) == False:
@@ -98,87 +105,50 @@ class ParcBatterieLithiumIon:
                 else:
                     prod = consigne
         
-        prix = self.calculPrix(prod)
+        prix = self.calculPrix(self.calculType(prod))*abs(prod)/100*self.PROD_MAX
         return (prod, prix)
     
     def simulation_destockage(self):
     
-        if self.compteur_surtension == 1 or self.compteur_pause < 4:
+        if self.compteur_fin == 1 or self.compteur_pause < 2 or self.activite > 0:
             '''cas où la batterie commence ou continue une pause due à une surtension'''
-            prod_min=0
-            prod_max=0
-            prix_min=0
-            prix_max=0
-            prix_normal=0
+            return (0.,0.,0.,0.,0.)
             
-        elif self.reste >= self.PROD_MAX/6:
-            '''cas idéal où il reste assez pour décharger à volonté'''
-            prix_min = self.COUT_MIN*self.PROD_MIN/6
-            prod_min = self.PROD_MIN/self.PROD_MAX*100
-            prix_max = self.COUT_MAX*self.PROD_MAX/6
-            prod_max = 100
-            prix_normal = self.calculPrix(self.activite)
-
+        elif self.coefficient_de_charge == 0:
+            prix_min = self.calculDecharge(1/4)[2]
+            prix_normal = self.calculDecharge(1/2)[3]
+            prix_max = self.calculDecharge(1)[4]
+            prod_min = self.calculDecharge(1/4)[0]
+            prod_max = self.calculDecharge(1)[1]
+            return (prod_min, prod_max, prix_min, prix_normal, prix_max)
+            
+            '''cas où la pile suit un régime constant'''
         else:
-            '''cas où il faut prendre en compte le reste'''
-            prix_min = self.COUT_MIN*self.PROD_MIN/6
-            prod_min = self.PROD_MIN/self.PROD_MAX*100
-            activite = abs(self.activite)
-            activite_maximum = self.reste/(self.PROD_MAX/6)*100
-            prod_max = activite_maximum
-            prix_max = self.calculPrix(activite_maximum)
-            if activite_maximum > activite:
-                prix_normal = self.calculPrix(activite)
-            else:
-                prix_normal = prix_max
-        if prod_min > prod_max:
-            prod_max = prod_min
-            prix_max = prix_min
-            prix_normal = prix_min
-            
-        return (-prod_min,-prod_max, prix_min, prix_normal, prix_max)
+                return self.calculDecharge(self.coefficient_de_charge)
     
     def simulation_stockage(self):
     
-        if self.compteur_surtension == 1 or self.compteur_pause < 4:
-            '''cas où la batterie commence ou continue une pause due à une surtension'''
-            prod_min=0
-            prod_max=0
-            prix_min=0
-            prix_max=0
-            prix_normal=0
+        if self.compteur_fin == 1 or self.compteur_pause < 2 or self.activite < 0:
+            '''cas où la batterie commence ou continue une pause due à une surtension ou à une transition après une décharge'''
+            return (0.,0.,0.,0.,0.)
+        
+            '''cas où la pile sort d'une pause et où elle peut choisir son mode de stockage'''
+        elif self.coefficient_de_charge == 0:
+            prix_min = self.calculCharge(1/4)[2]
+            prix_normal = self.calculCharge(1/2)[3]
+            prix_max = self.calculCharge(1)[4]
+            prod_min = self.calculCharge(1/4)[0]
+            prod_max = self.calculCharge(1)[1]
+            return (prod_min, prod_max, prix_min, prix_normal, prix_max)
             
-        elif self.capacite-self.reste>=self.PROD_MAX/6:
-            '''cas idéal où il reste assez pour charger à volonté'''
-            prix_min = self.COUT_MIN*self.PROD_MIN/6
-            prod_min = self.PROD_MIN/self.PROD_MAX*100
-            prix_max = self.COUT_MAX*self.PROD_MAX/6
-            prod_max = 100
-            prix_normal = self.calculPrix(self.activite)
-            
+            '''cas où la pile suit un régime constant'''
         else:
-            '''cas où il faut prendre en compte le reste'''
-            prix_min = self.COUT_MIN*self.PROD_MIN/6
-            prod_min = self.PROD_MIN/self.PROD_MAX*100
-            activite_maximum = (self.capacite-self.reste)/(self.PROD_MAX/6)*100
-            prod_max = activite_maximum
-            prix_max = self.calculPrix(activite_maximum)
-            if activite_maximum > abs(self.activite):
-                prix_normal = self.calculPrix(self.activite)
-            else:
-                prix_normal = prix_max
-                
-        if prod_min > prod_max:
-            if prod_min > prod_max:
-                prod_max = prod_min
-                prix_max = prix_min
-                prix_normal = prix_min
-            
-        return (prod_min, prod_max, prix_min, prix_normal, prix_max)    
+                return self.calculCharge(self.coefficient_de_charge)    
+        
     
     def contraintes(self,consigne): #consigne en pourcentage
         
-        if self.compteur_surtension == 1 or self.compteur_pause < 4: #cas de la mise en pause
+        if self.compteur_fin == 1 or self.compteur_pause < 2: #cas de la mise en pause
             if consigne == 0:
                 return True
             else:
@@ -186,7 +156,9 @@ class ParcBatterieLithiumIon:
         
         if consigne > 0: #cas du stockage
             if consigne >= self.simulation_stockage()[0] and consigne <= self.simulation_stockage()[1]:
-                return True
+                '''on vérifie que la consigne correspond à un système de charge possible'''
+                if (consigne >= self.calculCharge(self.choix_possibles_de_charge[2])[0] and consigne <= self.calculCharge(self.choix_possibles_de_charge[2])[1]) or (consigne >= self.calculCharge(self.choix_possibles_de_charge[1])[0] and consigne <= self.calculCharge(self.choix_possibles_de_charge[1])[1]) or (consigne >= self.calculCharge(self.choix_possibles_de_charge[0])[0] and consigne <= self.calculCharge(self.choix_possibles_de_charge[0])[1]):
+                    return True
             else:
                 return False
             
@@ -198,34 +170,122 @@ class ParcBatterieLithiumIon:
         else: #non activité
             return True
             
-    def calculPrix(self, activite):
+    def calculPrix(self, type_de_charge):
         '''cas surtension'''
-        if abs(activite)/100*self.PROD_MAX > self.PROD_NOR:
-            prix = ((self.COUT_MAX-self.COUT_NOR)*(activite/100)^100 + self.COUT_NOR)*self.PROD_MAX/6
-            '''cas normal'''
+        if type_de_charge == self.choix_possibles_de_charge[0]:
+            prix = self.cout_charge_rapide
+            '''cas semi rapide'''
+        elif type_de_charge == self.choix_possibles_de_charge[1]:
+            prix = self.cout_charge_semi_rapide
+            '''cas charge lente'''
+        elif type_de_charge == self.choix_possibles_de_charge[2]:
+            prix = self.cout_charge_lente
         else:
-            prix = self.COUT_NOR*activite/100*self.PROD_MAX/6
+            prix = 0
         return prix
 
-'''vous êtes arrivé au bout félicitations !'''
-
+    def calculCharge(self, type_de_charge): #calcul les possibilités en fonction du type de charge choisi (rapide, semi-rapide ou lente)
+        
+        if self.capacite-self.reste >= self.PROD_MAX*type_de_charge/6:
+            '''cas idéal où il reste assez pour charger à volonté'''
+            prix_min = self.calculPrix(type_de_charge)*self.PROD_MIN*type_de_charge
+            prod_min = self.PROD_MIN*type_de_charge/self.PROD_MAX*100
+            prix_max = self.calculPrix(type_de_charge)*self.PROD_MAX*type_de_charge
+            prod_max = 100*type_de_charge
+            prix_normal = self.calculPrix(type_de_charge)*self.activite/100*self.PROD_MAX
+            
+        else:
+            '''cas où il faut prendre en compte le reste'''
+            prix_min = self.calculPrix(type_de_charge)*self.PROD_MIN*type_de_charge
+            prod_min = self.PROD_MIN/self.PROD_MAX*100
+            activite_maximum = (self.capacite-self.reste)/(self.PROD_MAX/6)*100
+            prod_max = activite_maximum
+            prix_max = self.calculPrix(type_de_charge)*self.PROD_MAX*type_de_charge
+            if activite_maximum > abs(self.activite):
+                prix_normal = self.calculPrix(type_de_charge)*self.activite/100*self.PROD_MAX
+            else:
+                prix_normal = prix_max
+                
+        if prod_min > prod_max:
+            prod_max = prod_min
+            prix_max = prix_min
+            prix_normal = prix_min
+        
+        return (prod_min, prod_max, prix_min, prix_normal, prix_max)
+    
+    def calculDecharge(self, type_de_charge): #calcul les possibilités en fonction du type de charge choisi (rapide, semi-rapide ou lente)
+        
+        if self.reste >= self.PROD_MAX*type_de_charge/6:
+            '''cas idéal où il reste assez pour décharger à volonté'''
+            prix_min = self.calculPrix(type_de_charge)*self.PROD_MIN*type_de_charge
+            prod_min = self.PROD_MIN*type_de_charge/self.PROD_MAX*100
+            prix_max = self.calculPrix(type_de_charge)*self.PROD_MAX*type_de_charge
+            prod_max = 100*type_de_charge
+            prix_normal = self.calculPrix(type_de_charge)*self.activite/100*self.PROD_MAX
+            
+        else:
+            '''cas où il faut prendre en compte le reste'''
+            prix_min = self.calculPrix(type_de_charge)*self.PROD_MIN*type_de_charge
+            prod_min = self.PROD_MIN/self.PROD_MAX*100
+            activite_maximum = self.reste/(self.PROD_MAX/6)*100
+            prod_max = activite_maximum
+            prix_max = self.calculPrix(type_de_charge)*self.PROD_MAX*type_de_charge
+            if activite_maximum > abs(self.activite):
+                prix_normal = self.calculPrix(type_de_charge)*self.activite/100*self.PROD_MAX
+            else:
+                prix_normal = prix_max
+                
+        if prod_min > prod_max:
+            prod_max = prod_min
+            prix_max = prix_min
+            prix_normal = prix_min
+        
+        return (-prod_min, -prod_max, prix_min, prix_normal, prix_max)
+        
+    def calculType(self, activite):
+        min = 1000.
+        type_de_charge = 0
+        puissance = abs(activite)/100*self.PROD_MAX
+        for i in self.choix_possibles_de_charge:
+            puissance_normale = i*self.PROD_NOR
+            tmp = abs(puissance-puissance_normale)
+            if tmp < min:
+                min = tmp
+                type_de_charge = i
+        return type_de_charge
+        
     #pour les tests
 if __name__=='__main__':
-    a = ParcBatterieLithiumIon(activite=100, prop=1/5)
+    a = ParcBatterieLithiumIon(activite=0, prop=1/5)
+    print(a.capacite)
     print(a.reste)
     print(a.contraintes(70.))
     print(a.prevision(70.))
     print(a.simulation_stockage())
     print(a.simulation_destockage())
-    print(a.compteur_surtension)
+    print(a.compteur_fin)
     a.etat_suivant(70)
+    print(a.type_de_charge)
     print(a.reste)
     print(a.contraintes(-100))
     print(a.prevision(-100))
     print(a.simulation_stockage())
     print(a.simulation_destockage())
-    print(a.compteur_surtension)
+    print(a.compteur_fin)
     a.etat_suivant(-100)
     print(a.reste)
-    print(a.compteur_surtension)
-    
+    print(a.compteur_fin)
+    a.etat_suivant(-100)
+    print(a.compteur_fin)
+    print(a.compteur_pause)
+    a.etat_suivant(-100)
+    print(a.compteur_pause)
+    a.etat_suivant(-100)
+    print(a.activite)
+    print(a.compteur_pause)
+    print(a.reste)
+    print(a.contraintes(-70.))
+    print(a.prevision(-70.))
+    print(a.simulation_stockage())
+    print(a.simulation_destockage())
+    print(a.compteur_fin)
